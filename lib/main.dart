c8d69bd49fcab1fa24e9e48e7fc7a61b2b3b512f
Robert Wood Jr <rdrwood@gmail.com>
2020-01-26 21:20:39 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        appBar: AppBar(
+          title: Text('Rob\'s App!'),
+          backgroundColor: Colors.red[600],
+        ),
+        body: Image(
+
+          image: NetworkImage('http://www.rdkwood.com/images/profPic.jpg'),
+        ),
+      ),
+    ),
+  );
+}
